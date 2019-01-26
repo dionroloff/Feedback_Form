f7b71c92b5ccfd030188f229659511eab42aca95
Dion Roloff <dion.roloff@gmail.com>
2019-01-26 11:46:56 -0600

    split review component into smaller components

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Review from './../Review/Review.js';
 
 class Feelings extends Component {
 
-
+    render() {
+        return(
+            <Review /> 
+        )
+    }
     
 }
 
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,15 +2,19 @@
 // This will be the current redux values displayed on the DOM
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import ReviewFeelings from './ReviewFeelings.js';
+import ReviewUnderstanding from './ReviewUnderstanding.js';
+import ReviewSupport from './ReviewSupport.js';
+import ReviewComments from './ReviewComments';
 
 class Review extends Component {
     render() {
         return(
             <div>
-                <h3>Feelings:</h3>
-                <h3>Understanding:</h3>
-                <h3>Support:</h3>
-                <h3>Comments:</h3>
+                <ReviewFeelings />
+                <ReviewUnderstanding />
+                <ReviewSupport />
+                <ReviewComments />
                 <button>Incomplete</button>
             </div> 
         )
