3839ffe1c5cedcedd47cf2cf524e0da833976f24
Dion Roloff <dion.roloff@gmail.com>
2019-01-28 16:04:00 -0600

    deploy app

---
+++ build/precache-manifest.e1b1fe2e7e2992cee5cf0811ef581510.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71dacb15903f40d77373",
+    "url": "/static/css/main.f4706ca9.chunk.css"
+  },
+  {
+    "revision": "71dacb15903f40d77373",
+    "url": "/static/js/main.71dacb15.chunk.js"
+  },
+  {
+    "revision": "9818171edf3e2e200275",
+    "url": "/static/js/1.9818171e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "24ce1b81fcc167ddaacceacf291971e2",
+    "url": "/index.html"
+  }
+];
